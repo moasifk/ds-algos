45cbeb9f04654a255a7789e716238945b15f65af
MohammedAsif Kalathingal EXT <ext-mohammedasif.kalathingal@elisa.fi>
2025-04-24 10:52:38 +0300

    Python list operations

---
+++ leetcode/twosum.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        num_map = {}  # to store number and its index
+
+        for i, num in enumerate(nums):
+            complement = target - num
+            if complement in num_map:
+                return [num_map[complement], i]
+            num_map[num] = i
+
+
+solution = Solution()
+print(solution.twoSum([2, 4, 6, 7], 13)) 
